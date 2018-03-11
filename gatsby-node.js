764ca075197ee7400a3b22fffe5a9caa40be8efa
Peter Murphy <peterfrancismurphy@gmail.com>
2018-03-11 00:51:09 -0500

    Phew! Got the Blog Post working, but not the content just the

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,40 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
+const postTemplate = path.resolve(
+  "src/templates/post.js"
+)
 
- // You can delete this file if you're not using it
+exports.createPages = ({
+  boundActionCreators,
+  graphql
+}) => {
+  const { createPage } = boundActionCreators
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            html
+            id
+            frontmatter {
+              path
+              title
+            }
+          }
+        }
+      }
+    }
+  `).then(res => {
+    if (res.errors) {
+      console.log(res)
+      return Promise.reject(res.errors)
+    }
+    console.log(res.data.allMarkdownRemark.edges)
+    res.data.allMarkdownRemark.edges.forEach(
+      ({ node }) =>
+        createPage({
+          path: node.frontmatter.path,
+          component: postTemplate
+        })
+    )
+  })
+}
